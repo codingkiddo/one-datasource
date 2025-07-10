8ccaf71a86b04f42a5471a69c2ebd072578f0432
codingkiddo <codingkiddo@gmail.com>
2025-07-10 18:30:48 +0530

    modified:   src/main/java/one/datasource/ConnectionWrapper.java 	new file:   src/main/java/one/datasource/DataSourceImpl.java 	new file:   src/main/java/one/datasource/DataSourceMXBean.java

---
+++ src/main/java/one/datasource/DataSourceMXBean.java
@@ -0,0 +1,18 @@
+package one.datasource;
+
+public interface DataSourceMXBean {
+	
+	String getUrl();
+
+	int getOpenConnections();
+
+	int getIdleConnections();
+
+	int getTransactions();
+
+	int getMaxConnections();
+
+	long getBorrowTimeout();
+
+	long getLockTimeout();
+}
